759c4b4714304ca4776934989b0f8677cc8d34dd
Richard Wilkes <wilkes@me.com>
2018-11-18 09:16:04 -0800

    Ensure gocef prefix used on all native code

--- browser.h
+++ browser.h
@@ -1,5 +1,5 @@
-#ifndef BROWSER_H_
-#define BROWSER_H_
+#ifndef GOCEF_BROWSER_H_
+#define GOCEF_BROWSER_H_
 #pragma once
 
 #include "include/capi/cef_browser_capi.h"
@@ -7,4 +7,4 @@
 cef_browser_host_t *gocef_get_browser_host(cef_browser_t *browser);
 cef_frame_t *gocef_get_focused_frame(cef_browser_t *browser);
 
-#endif // BROWSER_H_
+#endif // GOCEF_BROWSER_H_
